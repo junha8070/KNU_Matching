325e67c31264328366878248f0ae63de384fb6b4
junha8070 <junha5021301@kakao.com>
2021-08-28 22:59:40 +0900

    Login_Activity 자바 수정

--- app/src/main/java/com/example/knu_matching/Login_Activity.java
+++ app/src/main/java/com/example/knu_matching/Login_Activity.java
@@ -3,12 +3,26 @@ package com.example.knu_matching;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
+
+    private Button btn_login, btn_IdFind, btn_PwdFind;
+    private EditText edt_Email, edt_Pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_IdFind = findViewById(R.id.btn_IdFind);
+        btn_PwdFind = findViewById(R.id.btn_PwdFind);
+
+        edt_Email = findViewById(R.id.edt_Email);
+        edt_Pwd = findViewById(R.id.edt_pwd);
+
+
     }
 }
