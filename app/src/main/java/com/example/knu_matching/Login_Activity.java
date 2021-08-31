4025a5c30a9eb287ab4f7a54f5da3f81bd4e32ba
junha8070 <junha5021301@kakao.com>
2021-08-31 14:38:04 +0900

    2021-08-31-14:37

--- app/src/main/java/com/example/knu_matching/Login_Activity.java
+++ app/src/main/java/com/example/knu_matching/Login_Activity.java
@@ -2,13 +2,15 @@ package com.example.knu_matching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
 
-    private Button btn_login, btn_IdFind, btn_PwdFind;
+    private Button btn_login, btn_IdFind, btn_PwdFind, btn_Register;
     private EditText edt_Email, edt_Pwd;
 
     @Override
@@ -19,10 +21,18 @@ public class Login_Activity extends AppCompatActivity {
         btn_login = findViewById(R.id.btn_login);
         btn_IdFind = findViewById(R.id.btn_IdFind);
         btn_PwdFind = findViewById(R.id.btn_PwdFind);
+        btn_Register = findViewById(R.id.btn_Register);
 
         edt_Email = findViewById(R.id.edt_Email);
         edt_Pwd = findViewById(R.id.edt_pwd);
 
+        btn_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Activity.this, Register_Activity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/knu_matching/Register_Activity.java
+++ app/src/main/java/com/example/knu_matching/Register_Activity.java
@@ -1,9 +1,18 @@
 package com.example.knu_matching;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +32,7 @@ public class Register_Activity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseReference;
     private EditText edt_Email, edt_password;
-    private Button btn_finish;
+    private Button btn_finish, btn_certificate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,14 @@ public class Register_Activity extends AppCompatActivity {
         edt_Email = findViewById(R.id.edt_Email);
         edt_password = findViewById(R.id.edt_Password);
         btn_finish = findViewById(R.id.btn_registerButton);
+        btn_certificate = findViewById(R.id.btn_knuID);
+
+        btn_certificate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MoveActivity();
+            }
+        });
 
         btn_finish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,11 +60,11 @@ public class Register_Activity extends AppCompatActivity {
                 String strEmail = edt_Email.getText().toString();
                 String strPassword = edt_password.getText().toString();
 
-                mFirebaseAuth.createUserWithEmailAndPassword(strEmail,strPassword).addOnCompleteListener
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPassword).addOnCompleteListener
                         (Register_Activity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
+                                if (task.isSuccessful()) {
                                     FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
                                     UserAccount account = new UserAccount();
                                     account.setIdToken(firebaseUser.getUid());
@@ -55,10 +72,10 @@ public class Register_Activity extends AppCompatActivity {
                                     account.setPassword(strPassword);
 
                                     mDatabaseReference.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
-                                    Toast.makeText(Register_Activity.this, "성공",Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(Register_Activity.this, "성공", Toast.LENGTH_SHORT).show();
 
                                 } else {
-                                    Toast.makeText(Register_Activity.this, "실패",Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(Register_Activity.this, "실패", Toast.LENGTH_SHORT).show();
 
                                 }
                             }
@@ -67,4 +84,21 @@ public class Register_Activity extends AppCompatActivity {
         });
 
     }
+
+    public void MoveActivity() {
+        Intent intent = new Intent(Register_Activity.this, Student_Certificate.class);
+        startActivityResult.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Log.d(TAG, "Back to Register Activity");
+                    }
+                }
+            });
+
 }
