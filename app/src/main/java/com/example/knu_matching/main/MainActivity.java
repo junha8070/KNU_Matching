cad57a1114a52d0509876eeb9c5840b2d773bc8a
junha8070 <junha5021301@kakao.com>
2021-09-10 00:28:55 +0900

    2021-09-09 23:49 세션유지

--- app/src/main/java/com/example/knu_matching/main/MainActivity.java
+++ app/src/main/java/com/example/knu_matching/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.knu_matching.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
@@ -8,18 +9,30 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.knu_matching.R;
+import com.example.knu_matching.UserAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button btn_register, btn_logout;
-    private FirebaseAuth mFirebaseAuth;
     private final String TAG = this.getClass().getSimpleName();
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private String uid = user != null ? user.getUid() : null;
+    private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     Context mContext;
+    protected String strEmail, strPassword, strNick, strMaojr, strStudentId;
 
     private ViewPager2 mViewPager;
     private MyViewPagerAdapter myPagerAdapter;
@@ -32,10 +45,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
         mContext = MainActivity.this;
         getSupportActionBar().hide();
+
+        db.collection("Account")
+                .document(user.getEmail().replace(".",">"))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            UserAccount userAccount = document.toObject(UserAccount.class);
+                            strEmail = userAccount.getEmailId();
+                            strPassword = userAccount.getPassword();
+                            strNick = userAccount.getNickName();
+                            strMaojr = userAccount.getMajor();
+                            strStudentId = userAccount.getStudentId();
+                            Toast.makeText(MainActivity.this, strMaojr,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
         //code = getIntent().getExtras().getString("code"); // 다른 Activity에서 값을 넘겨 받았을 때
         code = "";
