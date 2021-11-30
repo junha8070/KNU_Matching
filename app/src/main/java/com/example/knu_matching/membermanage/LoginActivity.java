ec9ef3e5b036bf0f99c8286bab3042ab445154af
koyulim <kul2862@naver.com>
2021-12-01 01:11:16 +0900

    로그인 비밀번호 수정

--- app/src/main/java/com/example/knu_matching/membermanage/LoginActivity.java
+++ app/src/main/java/com/example/knu_matching/membermanage/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
